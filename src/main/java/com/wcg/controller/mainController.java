2c05197fa96808faa6290e8737aef6eba6fb7f3a
cheai-ck <478610688@qq.com>
2019-12-14 19:33:27 +0800

    添加了静态文件

--- src/main/java/com/wcg/controller/mainController.java
+++ src/main/java/com/wcg/controller/mainController.java
@@ -6,10 +6,7 @@ import com.wcg.util.KeyUtil;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
@@ -236,4 +233,67 @@ public String outOrder(@RequestBody OrderDO order){
  public String indd(){
  return "admin-index";
 }
+
+/**
+ * 修改密码界面
+ */
+@RequestMapping("admin-gallery")
+public String gallery(){
+ return "admin-gallery";
+}
+ /**
+  * 登录验证
+  * @param managementUser
+  * @param managementPass
+  * @param code
+  * @param request
+  * @return
+  * @throws BusinessException
+  */
+/* @RequestMapping(value = "/check",method = {RequestMethod.POST}, consumes = {"application/x-www-form-urlencoded"})
+ @ResponseBody
+ public CommonReturnType check(@RequestParam(value = "managementUser") String managementUser,
+                               @RequestParam(value = "managementPass") String managementPass,
+                               @RequestParam(value = "code")String code, HttpServletRequest request) throws BusinessException {
+  String c =(String)request.getSession().getAttribute("verifyCode");
+  if (!c.toLowerCase().equals(code.toLowerCase())){
+   throw new BusinessException(EmBusinessError.CODE_ERROR);
+  }
+  //密码md5加密为密文
+  String md5Pass =Md5.md5(managementPass);
+  AdminDO adminDO=adminService.selectLogin(managementUser,md5Pass);
+  if (adminDO==null){
+   throw new BusinessException(EmBusinessError.USER_NOT_EXIST);
+  }
+
+  request.getSession().setAttribute("admin",adminDO.getManagementUser());
+  return CommonReturnType.create(adminDO);
+ }*/
+
+ /**
+  * 修改密码
+  * @param request
+  * @param managementPass 原密码
+  * @param password 新密码
+  * @return
+  * @throws BusinessException
+  */
+/* @RequestMapping(value = "/changepwd",method = {RequestMethod.POST}, consumes = {"application/x-www-form-urlencoded"})
+ @ResponseBody
+ public CommonReturnType changePass(HttpServletRequest request, @RequestParam(value = "managementPass")String managementPass, @RequestParam(value = "password")String password) throws BusinessException {
+  String managementUser = (String)request.getSession().getAttribute("admin");
+  String md5Pass = Md5.md5(managementPass);
+  AdminDO adminDO=adminService.selectLogin(managementUser,md5Pass);
+  if (adminDO==null){
+   throw new BusinessException(EmBusinessError.USER_NOT_EXIST);
+  }
+  String md5Password =Md5.md5(password);
+  int i = adminService.updatePass(managementUser,md5Password);
+  if (i==0){
+   throw new BusinessException(EmBusinessError.UNKNOWN_ERROR);
+  }
+  return CommonReturnType.create(adminDO);
+ }*/
+
+
 }
